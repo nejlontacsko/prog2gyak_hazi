883b8aa7ba0e2aaf3c4a63e7d63b590b8cbcfc1b
Fodor Attila <atti@fodyweb.hu>
2021-03-18 21:23:04 +0100

    Add files via upload
    - bugfix on row selection
    - added Static_Bilnk and Static_SetCursorPos

--- exectry/exectry/lcd.h
+++ exectry/exectry/lcd.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#ifndef LCD_TBIRD3_H
-#define LCD_TBIRD3_H
+#ifndef LCD_H
+#define LCD_H
 
 #define F_CPU 8000000
 
@@ -38,38 +38,49 @@
 #define LCD_CMD_DDRAMAD 0x80
 
 #define LCD_EMPTY_LINE ((char[]){0x00})
-
+	
 #define LCD_FLAG_BIT _BV(7)
 #define LCD_CURR_ADD (~LCD_FLAG_BIT)
 
 #define Static_Print(l0, l1, l2, l3) \
-_Static_assert(STRLEN(l0) <= 17, "Line 0 is too long."); \
-_Static_assert(STRLEN(l1) <= 17, "Line 1 is too long."); \
-_Static_assert(STRLEN(l2) <= 17, "Line 2 is too long."); \
-_Static_assert(STRLEN(l3) <= 17, "Line 3 is too long."); \
-lcdPrintHun(l0, l1, l2, l3)
-
+   _Static_assert(STRLEN(l0) <= 17, "Line 0 is too long."); \
+   _Static_assert(STRLEN(l1) <= 17, "Line 1 is too long."); \
+   _Static_assert(STRLEN(l2) <= 17, "Line 2 is too long."); \
+   _Static_assert(STRLEN(l3) <= 17, "Line 3 is too long."); \
+   lcdPrintHun(l0, l1, l2, l3)
+   
 #define Static_Limit(l) \
-_Static_assert(l > 1 || l < 8, "Custom character limit must be in [1..8] interval."); \
-DO_PRAGMA(message( \
-"[Line " STRINGIFY(__LINE__) "] Limit sat to " #l ".")); \
-lcdSetLimit(l)
+   _Static_assert(l > 1 || l < 8, "Custom character limit must be in [1..8] interval."); \
+   DO_PRAGMA(message( \
+		"[Line " STRINGIFY(__LINE__) "] Limit sat to " #l ".")); \
+   lcdSetLimit(l)
+   
+#define Static_Blink(b) \
+	lcdSendCmd(LCD_CMD_DSPCTRL | _BV(2) | (b & 1))
+	
+#define Static_SetCursorPos(x, y) \
+	lcdSendCmd(LCD_CMD_DDRAMAD | (_lcdRowAddr(x) + y))
 
+#define Static_SetCursorRow(row) \
+	Static_SetCursorPos(row, 0)
+   
 #define Static_CustomChar(location, charmap) \
-lcdCreateChar(location, charmap);  \
-DO_PRAGMA(message( \
-"[Line " STRINGIFY(__LINE__) "] The '" #charmap "' character stored in DDRAM location " #location ".")); \
-lcdSetLimit(lcdGetLimit() - 1); \
-DO_PRAGMA(message("\tDynamic accented letter limit decreased by 1! (Max. 8)"))
-
+	lcdCreateChar(location, charmap);  \
+	DO_PRAGMA(message( \
+		"[Line " STRINGIFY(__LINE__) "] The '" #charmap "' character stored in DDRAM location " #location ".")); \
+	lcdSetLimit(lcdGetLimit() - 1); \
+	DO_PRAGMA(message("\tDynamic accented letter limit decreased by 1! (Max. 8)"))
+	
 #define Static_Clear() \
-lcdSendCmd(LCD_CMD_CLRDISP); \
-lcdSendCmd(LCD_CMD_RETHOME)
+	lcdSendCmd(LCD_CMD_CLRDISP); \
+	lcdSendCmd(LCD_CMD_RETHOME)
 
 void _setDataDirection(int);
 
 void    _writeRegister(int, uint8_t);
 uint8_t _readRegister(int);
+
+uint8_t	_lcdRowAddr(int);
 
 void    lcdWait4BF();
 
